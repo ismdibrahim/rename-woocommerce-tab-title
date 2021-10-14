b9ba1d15b5d2f87a229b193fdd6a9fc941d4bca4
Md. Ibrahim Hossen <48528670+ismdibrahim@users.noreply.github.com>
2021-10-14 11:25:30 +0600

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,10 @@
+add_filter( 'woocommerce_product_tabs', 'woo_rename_tabs', 98 );
+function woo_rename_tabs( $tabs ) {
+
+	$tabs['description']['title'] = __( 'More Information' );		// Rename the description tab
+	$tabs['reviews']['title'] = __( 'Ratings' );				// Rename the reviews tab
+	$tabs['additional_information']['title'] = __( 'Product Data' );	// Rename the additional information tab
+
+	return $tabs;
+
+}
